3f83c6405ce5eeb069f8f20a66c9803ada6792fc
bojanavasilevska <bojanavasilevska@gmail.com>
2023-06-21 10:58:58 +0200

    cleancode

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -13,12 +13,13 @@ interface Color {
 }
 
 function App() {
-  const [color, setColor] = useState<Color>({ hex: "#ffffff" });
-  const [colors, setColors] = useState<Color[]>([]);
-  const [showPicker, setShowPicker] = useState<boolean>(false);
-  const pickerRef = useRef<HTMLDivElement>(null);
+  const [color, setColor] = useState<Color>({ hex: "#ffffff" }); // State for the currently selected color
+  const [colors, setColors] = useState<Color[]>([]); // State for the list of colors
+  const [showPicker, setShowPicker] = useState<boolean>(false); // State to toggle color picker visibility
+  const pickerRef = useRef<HTMLDivElement>(null); // Reference to the color picker element
 
   useEffect(() => {
+    // Load colors from local storage on component mount
     const storedColors = localStorage.getItem("colors");
     if (storedColors) {
       setColors(JSON.parse(storedColors));
@@ -26,33 +27,40 @@ function App() {
   }, []);
 
   useEffect(() => {
+    // Save colors to local storage when colors state changes
     localStorage.setItem("colors", JSON.stringify(colors));
   }, [colors]);
 
   const handleColorChange = (newColor: ColorResult) => {
+    // Update the selected color when color picker value changes
     setColor({ hex: newColor.hex });
   };
 
   const handleAddColor = () => {
+    // Add the selected color to the colors list and hide the color picker
     setColors([...colors, color]);
     setShowPicker(false);
   };
 
   const handleDeleteColor = (index: number) => {
+    // Remove a color from the colors list based on its index
     const newColors = [...colors];
     newColors.splice(index, 1);
     setColors(newColors);
   };
 
   const handlePickerClick = (e: any) => {
+    // Hide the color picker when clicking outside of it
     if (pickerRef.current && !pickerRef.current.contains(e.target)) {
       setShowPicker(false);
     }
   };
 
   useEffect(() => {
+    // Add event listener to handle clicks outside of the color picker
     document.addEventListener("click", handlePickerClick, true);
     return () => {
+      // Clean up the event listener when the component unmounts
       document.removeEventListener("click", handlePickerClick, true);
     };
   }, []);
@@ -61,36 +69,47 @@ function App() {
     <div className="app-container">
       <div className="app-box">
         <h4 className="title">My Favorite Colors</h4>
+        
+        {/* Color input section */}
         <div className="color-section">
-  <div className="color-input-container">
-    <div className="heart-icon-container">
-      <FontAwesomeIcon
-        className="heart-icon"
-        icon={faHeart}
-        style={{ color: color.hex }}
-      />
-    </div>
-    <input
-      className="color-name"
-      type="text"
-      value={color.hex}
-      onChange={(e) => setColor({ hex: e.target.value })}
-      style={{ borderColor: color.hex }}
-    />
-  </div>
-  <button className="color-picker" onClick={() => setShowPicker(!showPicker)}>
-    <Box className="color-box" style={{ backgroundColor: color.hex }} />
-  </button>
-  <button className="button-add" onClick={handleAddColor}>
-    +
-  </button>
-</div>
+          <div className="color-input-container">
+            {/* Heart icon container */}
+            <div className="heart-icon-container">
+              <FontAwesomeIcon
+                className="heart-icon"
+                icon={faHeart}
+                style={{ color: color.hex }}
+              />
+            </div>
+            {/* Color input */}
+            <input
+              className="color-name"
+              type="text"
+              value={color.hex}
+              onChange={(e) => setColor({ hex: e.target.value })}
+              style={{ borderColor: color.hex }}
+            />
+          </div>
+          
+          {/* Color picker button */}
+          <button className="color-picker" onClick={() => setShowPicker(!showPicker)}>
+            <Box className="color-box" style={{ backgroundColor: color.hex }} />
+          </button>
+          
+          {/* Add color button */}
+          <button className="button-add" onClick={handleAddColor}>
+            +
+          </button>
+        </div>
 
+        {/* Color picker */}
         {showPicker && (
           <div className="color-picker-container" ref={pickerRef}>
             <ChromePicker color={color.hex} onChange={handleColorChange} />
           </div>
         )}
+
+        {/* Color list */}
         <ul className="color-list">
           {colors.map((c, i) => (
             <li
