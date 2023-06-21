fc12928232f1d445cb5f01e5fc9de44b7aeba8a1
bojanavasilevska <bojanavasilevska@gmail.com>
2023-06-21 10:44:44 +0200

    LayoutChange

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect, useRef } from "react";
 import "./App.css";
 
-import { faHeart, faPlus, faTrashCan } from "@fortawesome/free-solid-svg-icons";
+import { faHeart, faTrashAlt } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Box } from "@mantine/core";
 import { ChromePicker, ColorResult } from "react-color";
@@ -11,6 +11,7 @@ export const ENDPOINT = "http://localhost:8080";
 interface Color {
   hex: string;
 }
+
 function App() {
   const [color, setColor] = useState<Color>({ hex: "#ffffff" });
   const [colors, setColors] = useState<Color[]>([]);
@@ -57,62 +58,57 @@ function App() {
   }, []);
 
   return (
-    <Box className="app-box">
-      <div>
-        <div>
-          <h4 className="title">My favorite color</h4>
-        </div>
-        <div>
-          {colors.map((c, i) => (
-            <FontAwesomeIcon
-              className="icon"
-              icon={faHeart}
-              style={{ color: c.hex }}
-            />
-          ))}
-          <input
-            className="color-name"
-            type="text"
-            value={color.hex}
-            readOnly
-          />
+    <div className="app-container">
+      <div className="app-box">
+        <h4 className="title">My Favorite Colors</h4>
+        <div className="color-section">
+  <div className="color-input-container">
+    <div className="heart-icon-container">
+      <FontAwesomeIcon
+        className="heart-icon"
+        icon={faHeart}
+        style={{ color: color.hex }}
+      />
+    </div>
+    <input
+      className="color-name"
+      type="text"
+      value={color.hex}
+      onChange={(e) => setColor({ hex: e.target.value })}
+      style={{ borderColor: color.hex }}
+    />
+  </div>
+  <button className="color-picker" onClick={() => setShowPicker(!showPicker)}>
+    <Box className="color-box" style={{ backgroundColor: color.hex }} />
+  </button>
+  <button className="button-add" onClick={handleAddColor}>
+    +
+  </button>
+</div>
 
-          <button
-            className="color-picker"
-            onClick={() => setShowPicker(!showPicker)}
-          >
-            {colors.map((c, i) => (
-              <Box style={{ color: c.hex, display: "inline-block" }}></Box>
-            ))}
-          </button>
-
-          <button className="button-add" onClick={handleAddColor}>
-            <FontAwesomeIcon className="icon-plus" icon={faPlus} />
-          </button>
-        </div>
         {showPicker && (
-          <div ref={pickerRef}>
+          <div className="color-picker-container" ref={pickerRef}>
             <ChromePicker color={color.hex} onChange={handleColorChange} />
           </div>
         )}
-        <ul>
+        <ul className="color-list">
           {colors.map((c, i) => (
             <li
-              key={c.hex}
-              style={{ backgroundColor: c.hex, marginRight: 20, height: 35 }}
+              key={i}
+              className="color-list-item"
+              style={{ backgroundColor: c.hex }}
             >
               {c.hex}
-              <button
-                style={{ marginLeft: 400 }}
+              <FontAwesomeIcon
+                className="icon-trash"
+                icon={faTrashAlt}
                 onClick={() => handleDeleteColor(i)}
-              >
-                <FontAwesomeIcon className="icon-plus" icon={faTrashCan} />
-              </button>
+              />
             </li>
           ))}
         </ul>
       </div>
-    </Box>
+    </div>
   );
 }
 
